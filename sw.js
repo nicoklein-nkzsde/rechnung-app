95db2c0a865150f5735712c8b267f06b4e37c65a
nicoklein-nkzsde <nicoklein@nkzs.de>
2026-07-06 19:06:19 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nkzs-app-v1';
+const CACHE = 'nkzs-app-v2';
 const ASSETS = ['.', 'index.html', 'manifest.webmanifest', 'icon-180.png', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
